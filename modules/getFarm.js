06eb3252ad852ca4a238f5ab602faba6551eccfb
BrandonBradshaw <33530340+BrandonBradshaw@users.noreply.github.com>
2021-05-11 11:05:34 -0500

    Create getFarm.js

---
+++ modules/getFarm.js
@@ -0,0 +1,24 @@
+const fetch = require("node-fetch")
+
+
+// Getting the fetched content
+const getFarms = (baseUrl, accessToken) => {
+    return fetch(`${baseUrl}/farms`, {
+        method : 'GET',
+        headers: {
+            'Authorization': `Bearer ${accessToken}`,
+        }
+    }).then(r => {
+        if (!r.ok) {
+            r.json().then(data => {
+                console.error(data.message || 'Response error');
+            });
+            return Promise.reject(r);
+        }
+        else {
+            return r.json();
+        }
+    });
+}
+
+module.exports = getFarms;
